2884ae183843caa666f41361780e98e56ed9ff43
Rodrigo Souza <rodrigosein@gmail.com>
2022-02-20 13:37:22 -0300

    Add entity Classes

---
+++ src/main/java/com/project/professor/allocation/entity/Couse.java
@@ -0,0 +1,20 @@
+package com.project.professor.allocation.entity;
+
+public class Couse {
+
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+}
---
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -0,0 +1,5 @@
+package com.project.professor.allocation.entity;
+
+public class Department {
+
+}
