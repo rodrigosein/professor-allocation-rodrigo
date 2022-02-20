ad922335232b90e5fdf6bcf18e7df314176dfc3a
Rodrigo Souza <rodrigosein@gmail.com>
2022-02-20 11:21:00 -0300

    Adicionando projeto inicial

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationRodrigoseinApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationRodrigoseinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationRodrigoseinApplication.class, args);
+	}
+
+}
