3582bb1d2de4389a4e3f8d196b81e007c41a9d57
Rodrigo Souza <rodrigosein@gmail.com>
2022-02-24 18:17:51 -0300

    Criação RepositoryTest

---
+++ src/test/java/com/project/professor/allocation/repository/CourseRepositoryTest.java
@@ -0,0 +1,94 @@
+package com.project.professor.allocation.repository;
+
+import java.text.ParseException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
+
+public class CourseRepositoryTest {
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Test
+	public void findAll() {
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findById() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findByProfessorId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void findByCourseId() {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void save_create() throws ParseException {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void save_update() throws ParseException {
+		// Arrange
+
+		// Act
+
+		// Print
+
+	}
+
+	@Test
+	public void deleteById() {
+		// Arrange
+
+		// Act
+
+	}
+
+	@Test
+	public void deleteAll() {
+		// Act
+
+	}
+}
